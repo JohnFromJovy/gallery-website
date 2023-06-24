8b8dbec0516c610e15724aa4ca278bf58c04e7b6
Li Jiang <jiangli82@gmail.com>
2023-06-25 00:11:28 +1000

    Add load More button on gallery page

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -4,9 +4,11 @@ import "./picture.css";
 const Picture = ({ data }) => {
   return (
     <div className="picture">
-      <p>{data.photographer}</p>
       <div className="imageContainer">
         <img alt="" href=" " src={data.src.portrait}></img>
+      </div>
+      <div className="photographerName">
+        <p>{data.photographer}</p>
       </div>
     </div>
   );
--- src/components/Views/Gallery/Gallery.js
+++ src/components/Views/Gallery/Gallery.js
@@ -7,39 +7,35 @@ import Search from "../../Search/Search";
 function Gallery() {
   const [data, setData] = useState(null);
   const [input, setInput] = useState([]);
-  const [currentSearch, setCurrentSearch] = useState([]);
-  const [page, setPage] = useState(1);
+  const [currentSearch, setCurrentSearch] = useState("");
+  const [page, setPage] = useState(2);
   const client = createClient(
     "tP6ce7LPIrr9FcVbNMPrWLyGxlhxdD8h4Sdmxk5YK8OXHFRGUE2tLFRF"
   );
-  const fetchData = async () => {
+
+  const loadingSpinner = () => {
+    return <div className="spinner">Loading</div>;
+  };
+
+  const fetchData = async (page) => {
     try {
       const result = await client.photos.curated({ page: page, per_page: 15 });
       setData(result.photos);
-      setPage(page + 1);
+      result.photos.map((e) => {
+        console.log(e);
+      });
     } catch (e) {
       console.log("fetch error out", e);
     }
-    /* } else {
-      try {
-        const result = await client.photos.search({ query, per_page: 15 });
-        setData(result.photos);
-      } catch (e) {
-        console.log("error out", e);
-      }
-    } */
   };
 
-  const searchData = async (query, page) => {
+  const searchData = async (query) => {
     try {
       const result = await client.photos.search({
         query,
-        page: page,
         per_page: 15,
       });
       setData(result.photos);
-      setPage(page + 1);
-      // setCurrentSearch(query);
     } catch (e) {
       console.log("search error out", e);
     }
@@ -47,22 +43,57 @@ function Gallery() {
 
   //when page load up
   useEffect(() => {
-    fetchData();
+    fetchData(page);
   }, []);
 
   //when search button clicked
   useEffect(() => {
     if (currentSearch === "") {
-      //console.log("load more button clicked then search photos");
       fetchData();
     } else {
-      //console.log("load more button clicked then fetch data");
-      searchData(currentSearch);
+      searchData(currentSearch, page);
     }
   }, [currentSearch]);
 
+  //when load more button clicked
+  const loadMore = async () => {
+    console.log("load more button clicked");
+    setPage(page + 1);
+    console.log("page number is " + page);
+    if (currentSearch === "") {
+      console.log("input is empty");
+      try {
+        const result = await client.photos.curated({
+          page: page,
+          per_page: 15,
+        });
+        setData(data.concat(result.photos));
+        result.photos.map((e) => {
+          console.log(e);
+        });
+      } catch (e) {
+        console.log("load more pictures error out", e);
+      }
+    } else {
+      console.log(
+        `search more photos because we have search input, which is ${currentSearch}`
+      );
+      try {
+        const result = await client.photos.search({
+          query: currentSearch,
+          page: page,
+          per_page: 15,
+        });
+        setData(data.concat(result.photos));
+      } catch (e) {
+        console.log("load more search error out", e);
+      }
+    }
+  };
+
+  //how to add spinner when loading more pages?
   if (!data) {
-    return <div className="spinner">spinner</div>;
+    return <div className="spinner">Loading</div>;
   }
   return (
     <>
@@ -76,6 +107,9 @@ function Gallery() {
         {data.map((d) => {
           return <Picture data={d} />;
         })}
+      </div>
+      <div className="loadMore">
+        <button onClick={loadMore}>Load More</button>
       </div>
     </>
   );
