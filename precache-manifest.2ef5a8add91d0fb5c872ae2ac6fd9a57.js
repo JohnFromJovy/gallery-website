b883380c85a22e8aa97c4383c5a22ae92a996f36
Li Jiang <jiangli82@gmail.com>
2023-07-06 10:29:22 +1000

    Updates

---
+++ precache-manifest.2ef5a8add91d0fb5c872ae2ac6fd9a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fd9ec3098e3cd3af8e1",
+    "url": "/portfolio/static/css/main.7977bd78.chunk.css"
+  },
+  {
+    "revision": "3fd9ec3098e3cd3af8e1",
+    "url": "/portfolio/static/js/main.3fd9ec30.chunk.js"
+  },
+  {
+    "revision": "c69a4d7434a9654b94e1",
+    "url": "/portfolio/static/js/1.c69a4d74.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "53e1dfe4a2583cdeae285ba77620f384",
+    "url": "/portfolio/static/media/20220713-0019cs.53e1dfe4.jpg"
+  },
+  {
+    "revision": "3ea9e99e6861d68377ad4ea372d5c93b",
+    "url": "/portfolio/index.html"
+  }
+];
